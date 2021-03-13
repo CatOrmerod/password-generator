d446bfd7a871716a14d6efb910f942cc8f494f07
Cat Ormerod <catormerod@gmail.com>
2021-03-14 10:02:55 +1100

    variables set

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var uppercaseArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercaseArray = uppercaseArray.toLowerCase();
+var specialcharactersArray = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var numbersArray = "0123456789";
+
+var passwordLength = prompt("How many characters (8-128) would you like in your password?");
+var uppercaseIncluded = confirm("would you like to include uppercase characters?");
+var lowercaseIncluded = confirm("would you like to include lowercase characters?");
+var specialcharactersIncluded = confirm("Would you like to include special characters?");
+var numbersIncluded = confirm("Would you like to include numbers?");
+
+var userSelection = []
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
