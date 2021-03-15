f03413640343ed0602f818d01a29a25d4935ad07
Cat Ormerod <catormerod@gmail.com>
2021-03-15 17:59:18 +1100

    Alerts added, indentation corrected, arrays amended

--- script.js
+++ script.js
@@ -5,71 +5,70 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
 
-// variable arrays
-var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var specialcharactersArray = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","^","_","`","{","|","}","~"];
-var numbersArray = ["0","1","2","3","4","5","6","7","8","9"];
+  // variable arrays
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercase = uppercase.toLowerCase();
+  var uppercaseArray = uppercase.split('');
+  var lowercaseArray = lowercase.split('');
+  var specialcharactersArray = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "`", "{", "|", "}", "~"];
+  var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-// user selection prompts
+  // user selection prompts
+  do {  
+    var passwordLength = prompt("How many characters (8-128) would you like in your password?");
+  } while (passwordLength<8 || passwordLength>128);
+  
+  
+  do { 
+    var error = false
+    var uppercaseIncluded = confirm("would you like to include uppercase characters?");
+    var lowercaseIncluded = confirm("would you like to include lowercase characters?");
+    var specialcharactersIncluded = confirm("Would you like to include special characters?");
+    var numbersIncluded = confirm("Would you like to include numbers?");
+    if (!uppercaseIncluded && !lowercaseIncluded && !specialcharactersIncluded && !numbersIncluded) {
+      alert("You must include at least one criteria")
+      error=true
+    }
 
-var passwordLength = prompt("How many characters (8-128) would you like in your password?");
-var uppercaseIncluded = confirm("would you like to include uppercase characters?");
-var lowercaseIncluded = confirm("would you like to include lowercase characters?");
-var specialcharactersIncluded = confirm("Would you like to include special characters?");
-var numbersIncluded = confirm("Would you like to include numbers?");
+  } while (error)
+  
+  var userSelection = [];
 
-var userSelection = [];
 
-// user selection grouped
+  // user selection grouped
 
-if (uppercaseIncluded) {
-  userSelection = userSelection.concat(uppercaseArray);
+  if (uppercaseIncluded) {
+    userSelection = userSelection.concat(uppercaseArray);
+  }
+
+  if (lowercaseIncluded) {
+    userSelection = userSelection.concat(lowercaseArray);
+  }
+
+  if (specialcharactersIncluded) {
+    userSelection = userSelection.concat(specialcharactersArray);
+  }
+
+  if (numbersIncluded) {
+    userSelection = userSelection.concat(numbersArray);
+  }
+  console.log(userSelection);
+
+  //loop for generating password
+  passwordFinal = [];
+
+  for (var i = 0; i < passwordLength; i++) {
+    var passwordSelection = userSelection[Math.floor(Math.random() * userSelection.length)];
+    passwordFinal.push(passwordSelection);
+  }
+  console.log(passwordFinal);
+  return passwordFinal.join("");
+
 }
-
-if (lowercaseIncluded) {
-  userSelection = userSelection.concat(lowercaseArray);
-}
-
-if (specialcharactersIncluded) {
-  userSelection = userSelection.concat(specialcharactersArray);
-}
-
-if (numbersIncluded) {
-  userSelection = userSelection.concat(numbersArray);
-}
-console.log(userSelection);
-
-//loop for generating password
-passwordFinal = [];
-
-for (var i = 0; i < passwordLength; i++) {
-  var passwordSelection = userSelection[Math.floor(Math.random() * userSelection.length)];
-  passwordFinal.push(passwordSelection);
-}
-console.log(passwordFinal);
-return passwordFinal.join("");
-
-}
-
-//Alerts
-
-//if (!passwordLength) {
-// alert("Please enter a value between 8-128");
-//}
-
-//else if (passwordLength <8 || passwordLength>128 ) {
-//  passwordLength = prompt("Please enter a value between 8-128");
-//  
-//if (!uppercaseIncluded && !lowercaseIncluded && !specialcharactersIncluded && !numbersIncluded) {
-//  userSelection = alert("You must include at least one criteria");
-//}
