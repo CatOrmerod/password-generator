48a52b40960f89fcee09d2e113b7cfc8c388bf27
Cat Ormerod <catormerod@gmail.com>
2021-03-15 12:40:02 +1100

    Password Function returning password, need to add alerts and verification

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// variables
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+
+// variable arrays
 var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var specialcharactersArray = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","^","_","`","{","|","}","~"];
 var numbersArray = ["0","1","2","3","4","5","6","7","8","9"];
+
+// user selection prompts
 
 var passwordLength = prompt("How many characters (8-128) would you like in your password?");
 var uppercaseIncluded = confirm("would you like to include uppercase characters?");
@@ -15,14 +30,7 @@ var numbersIncluded = confirm("Would you like to include numbers?");
 
 var userSelection = [];
 
-// User Selection
-if (!passwordLength) {
-  alert("Please enter a value between 8-128");
-}
-
-if (passwordLength <8 || passwordLength>128 ) {
-  passwordLength = prompt("Please enter a value between 8-128");
-}
+// user selection grouped
 
 if (uppercaseIncluded) {
   userSelection = userSelection.concat(uppercaseArray);
@@ -41,19 +49,27 @@ if (numbersIncluded) {
 }
 console.log(userSelection);
 
-if (!uppercaseIncluded && !lowercaseIncluded && !specialcharactersIncluded && !numbersIncluded) {
-  userSelection = alert("You must include at least one criteria");
+//loop for generating password
+passwordFinal = [];
+
+for (var i = 0; i < passwordLength; i++) {
+  var passwordSelection = userSelection[Math.floor(Math.random() * userSelection.length)];
+  passwordFinal.push(passwordSelection);
+}
+console.log(passwordFinal);
+return passwordFinal.join("");
+
 }
 
+//Alerts
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//if (!passwordLength) {
+// alert("Please enter a value between 8-128");
+//}
 
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//else if (passwordLength <8 || passwordLength>128 ) {
+//  passwordLength = prompt("Please enter a value between 8-128");
+//  
+//if (!uppercaseIncluded && !lowercaseIncluded && !specialcharactersIncluded && !numbersIncluded) {
+//  userSelection = alert("You must include at least one criteria");
+//}
